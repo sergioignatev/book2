3d89457c3f3729c688ca5bcbcc93aec53ac0a554
sergioIgnatev <sergioignatev17@gmail.com>
2022-08-25 00:37:18 +0300

    hh

--- scripts.js
+++ scripts.js
@@ -11,5 +11,14 @@ function loadCart() {
     xhttp.open("GET", "carthagee.html");
     xhttp.send();
   }
+  function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("livius").innerHTML = this.responseText;
+     
+    }
+    xhttp.open("GET", "livius.txt");
+    xhttp.send();
+  }
 document.querySelector("button").innerHTML=main
 document.querySelector("button").addEventListener("click",loadCart)
